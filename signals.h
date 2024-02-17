af0a760c07aa08c571d5596bbc3179add7d63656
Nadavas <117817174+Nadavas@users.noreply.github.com>
2024-02-17 20:44:09 +0200

    Update signals.h

--- signals.h
+++ signals.h
@@ -1,20 +1,11 @@
 #ifndef _SIGS_H
 #define _SIGS_H
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h> 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <string>
+#include <csignal>
 #include "commands.h"
-#define MAX_LINE_SIZE 80
-#define MAXARGS 20
-//using namespace std;
 
 void sig_handler(int sig_number);
 
 
 #endif
+
 
